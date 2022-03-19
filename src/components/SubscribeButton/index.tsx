3ee86deeb6ab85fc87bd2ac51aaeb221310870f0
Felipe Curcio <felipe_nieto010@hotmail.com>
2022-03-18 23:47:08 -0300

    stripe subscription and webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe_js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,5 +8,36 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  return <button className={styles.subscribeButton}>Subscribe now</button>
+  const { data } = useSession()
+
+  const handleSubscribe = async () => {
+    if (!data) {
+      signIn()
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      /*
+      docs
+      https://stripe.com/docs/js/checkout
+      https://stripe.com/docs/stripe-js/react
+      */
+
+      stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  return (
+    <button className={styles.subscribeButton} onClick={handleSubscribe}>
+      Subscribe now
+    </button>
+  )
 }
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,77 @@
+import { Console } from 'console'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import {
+  getItemUsers,
+  putItemUsers,
+  queryItemUsers,
+  updateItemUsers,
+} from '../../services/dynamodbProvider'
+import { stripe } from '../../services/stripe'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req })
+
+    /*
+    docs
+    https://stripe.com/docs/api/checkout/sessions/object
+    https://stripe.com/docs/checkout/quickstart
+    */
+
+    const queryItemParams = {
+      KeyConditionExpression: 'email = :s',
+      ExpressionAttributeValues: {
+        ':s': { S: session.user.email },
+      },
+      TableName: 'Users',
+    }
+
+    const { Items } = await queryItemUsers(queryItemParams)
+
+    const CustomerId = Items.find((item) => item.customerId)
+
+    let customerId = CustomerId?.customerId?.S
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        name: session.user.name,
+      })
+
+      const puItemParams = {
+        TableName: 'Users',
+        Key: {
+          email: { S: session.user.email },
+          subscriptionId: { S: `${null}` },
+        },
+        UpdateExpression: 'SET customerId = :c, subscriptionStatus = :s',
+        ExpressionAttributeValues: {
+          ':c': { S: stripeCustomer.id },
+          ':s': { S: 'to be subscribed' },
+        },
+        ReturnValues: 'ALL_NEW',
+      }
+
+      await updateItemUsers(puItemParams)
+
+      customerId = stripeCustomer.id
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [{ price: 'price_1K24gpDUdOkOs6FutpCtGcK2', quantity: 1 }],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
+}
